0da394285b5dd477e412c75930241cf6cde85b4b
AJAY KUMAR DHANDE <144995723+DHANDEAJAYKUMAR@users.noreply.github.com>
2025-12-11 10:37:02 +0530

    Update transaction.js

--- actions/transaction.js
+++ actions/transaction.js
@@ -175,7 +175,7 @@ export async function getUserTransactions(query = {}) {
 
 export async function scanReceipt(file) {
   try {
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
     const arrayBuffer = await file.arrayBuffer();
     const base64String = Buffer.from(arrayBuffer).toString("base64");
