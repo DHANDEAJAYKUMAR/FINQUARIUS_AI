dd8a8140605583c284053f5040249697667d487d
AJAY KUMAR DHANDE <144995723+DHANDEAJAYKUMAR@users.noreply.github.com>
2026-01-06 21:06:23 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
 
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
-              <p>© 2025 Ajay Kumar. All rights reserved.</p>
+              <p>© 2026 Ajay Kumar. All rights reserved.</p>
             </div>
           </footer>
         </body>
